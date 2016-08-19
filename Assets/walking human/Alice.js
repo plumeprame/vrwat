c911b5fb1a8e0c7ddc335ba60d2dfbf04ef8b972
Panarin Iamsongcram <plumei38@gmail.com>
2016-08-19 21:58:42 +0700

    test1
    ลอง

---
+++ Assets/walking human/Alice.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+ 
+var animator : Animator; //stores the animator component
+var v : float; //vertical movements
+var h : float; //horizontal movements
+var sprint : float;
+ 
+function Start () {
+animator = GetComponent("mime"); //assigns Animator component when we start the game 
+}
+ 
+function Update () {
+ 
+v = Input.GetAxis("Vertical");
+h = Input.GetAxis("Horizontal");
+Sprinting();
+}
+
+function fixedUpdate(){
+//set the "Walk" parameter to the v axis value
+animator.SetFloat ("Walking", v);
+animator.SetFloat ("Turning", h);
+animator.SetFloat("Sprinting", sprint);
+}
+ 
+function Sprinting () {
+if(Input.GetButton("Fire1")) {
+	sprint = 0.2;
+}
+else {
+	sprint = 0.0;
+}
+ 
+}
