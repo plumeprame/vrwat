1fee2b4c3e293baea382d600de7fbad72d2b4710
sujin <sujinchaichanamongkol@gmail.com>
2016-08-25 12:49:08 +0700

    dialog and patrol cs

---
+++ Assets/Patrol.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Patrol : MonoBehaviour {
+	static Animator anim;
+	public Transform goal;
+	public Transform player;
+	private NavMeshAgent agent;
+	void Start () {
+		 agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position;
+		anim = GetComponent<Animator>();
+
+	}
+	void Update (){
+		// Check if we've reached the destination
+
+		if (!agent.pathPending) {
+			if ( Vector3.Distance( agent.destination, agent.transform.position) <= agent.stoppingDistance)
+			{
+				if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f) {
+					Debug.Log ("reach");
+					anim.SetFloat ("Walking", 0.0f);
+					Vector3 direction = player.position - this.transform.position;
+//					direction.y = 0;
+					this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),0.1f);
+					Debug.Log ("here");
+				} 
+			}else {
+				Debug.Log ("walking");
+				anim.SetFloat ("Walking", 0.2f);
+			}
+		} 
+	}
+}
---
+++ Assets/ShowDiglog.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ShowDiglog : MonoBehaviour {
+	public int TextBoxOnCheck = 0;
+	public GameObject MessageBox;
+	public GameObject TextBox1; // Defines the text box as an object
+	public GameObject TextBox2; // Defines the text box as an object
+	public GameObject TextBox3; // Defines the text box as an object
+	public GameObject TextBox4;
+	public string dialog1;
+	public string dialog2;
+	public string dialog3;
+	public string dialog4;
+	public string[] Dialog;
+
+	string TextMessage1; // The text we can change
+	string TextMessage2;
+	string TextMessage3;
+	string TextMessage4;
+
+
+	// Use this for initialization
+	void Start () {
+		MessageBox.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown(){
+
+		if (TextBoxOnCheck == 0) {
+			Debug.Log ("Click Leaw!!");
+			TextBoxOnCheck = 1; //Sets the variable to turn the box off
+			MessageBox.SetActive(true); //Re-enables the box
+			TextMessage1 = dialog1;
+			TextMessage2 = dialog2;
+			TextMessage3 = dialog3;
+			TextMessage4 = dialog4;
+			TextBox1.GetComponent<Text>().text = TextMessage1; //changes the message
+			TextBox2.GetComponent<Text>().text = TextMessage2; //changes the message
+			TextBox3.GetComponent<Text>().text = TextMessage3; //changes the message
+			TextBox4.GetComponent<Text>().text = TextMessage4; //changes the message
+		}
+		else {
+			Debug.Log ("close");
+			TextBoxOnCheck = 0;
+			MessageBox.SetActive(false);
+
+		}
+	}
+}
